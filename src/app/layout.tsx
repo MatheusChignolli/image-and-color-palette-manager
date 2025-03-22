6d5f796fc37ab1dba1cb9372377fff2f2c710a34
Matheus Chignolli <matheuschignolli@gmail.com>
2025-03-22 14:56:12 -0300

    add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Toaster } from 'react-hot-toast'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 import Header from '@/components/header'
 import Footer from '@/components/footer'
@@ -49,6 +50,7 @@ export default function RootLayout({
           }}
         />
       </body>
+      <GoogleTagManager gtmId="G-5B4VQW42RF" />
     </html>
   )
 }
